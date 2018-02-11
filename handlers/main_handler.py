2f7f62728d72c302837424d0083928ff6fa0d657
fLauren <laurenfraser222@gmail.com>
2018-02-11 02:55:50 -0500

    mystuff

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -15,7 +15,10 @@ class MainHandler(webapp2.RequestHandler):
         	"title": "Main Title",
             "content": "Hello"
         }
-		
+
+
+		user = user.get_current_user()
+
 		template = jinja_env.env.get_template('templates/tmpl.html')
 		self.response.out.write(template.render(html_params))
 		
