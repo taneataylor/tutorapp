fd352e60bcf82c52b302d4d9d4cbdf54339e9e8d
fLauren <laurenfraser222@gmail.com>
2018-02-11 03:19:46 -0500

    mhl

--- handlers/studentmatch_handler.py
+++ handlers/studentmatch_handler.py
@@ -2,6 +2,7 @@
 import jinja_env
 import logging
 import webapp2
+from google.appengine.ext import ndb
 from google.appengine.api import users
 from models import newuser
 from models import tutoruser
@@ -9,12 +10,16 @@ from models import tutoruser
 class StudentMatchHandler(webapp2.RequestHandler):
     def get(self):
         logging.info("MainHandler")
+
+        template = jinja_env.env.get_template('templates/studentmatch.html')
+        self.response.out.write(template.render())
+
         user = users.get_current_user()
         if user == None: 
             self.redirect("/")
             return
         myUser = tutoruser.UserModel.query(newuser.UserModel.user_email == user.email()).get()
-        new_user = "<div>" + "Here are tutors that can help you with " + str(myUser.subject) + " on " + str(myUser.day) + " at " + str(myUser.time)</div>"
+        new_user = "<div>" + "Here are tutors that can help you with " + str(myUser.subject) + " on " + str(myUser.day) + " at " + str(myUser.time) + "</div>"
 
 
         matchedTutors = tutoruser.UserModel.query().fetch()
@@ -32,6 +37,4 @@ class StudentMatchHandler(webapp2.RequestHandler):
 ##This sorts the users from highest compatability to lowest
         matchedTutors.sort(key=lambda tutor: tutor.score, reverse = True)
 ##This takes out the user trying to find a roommate from the list of options
-        matchedTutors= matchedTutors[1:
-        template = jinja_env.env.get_template('templates/studentmatch.html')
-        self.response.out.write(template.render())
+        matchedTutors = matchedTutors[1:]
